7a9ac74fb770ed146095dd052af59c5da01a5c5c
Ali Ramazan Mert <ramazanmert98@gmail.com>
2020-10-24 20:56:00 +0300

    Updates

---
+++ precache-manifest.6e77b0d5c2134ca6bff07b70c481f974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c76a5349c414dc2f68fa0439fc5d5d",
+    "url": "/endemic-simulation/index.html"
+  },
+  {
+    "revision": "0b95d47a0f24a9282dc5",
+    "url": "/endemic-simulation/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "343030eae583ca84dceb",
+    "url": "/endemic-simulation/static/css/main.c2aed115.chunk.css"
+  },
+  {
+    "revision": "0b95d47a0f24a9282dc5",
+    "url": "/endemic-simulation/static/js/2.b0b55d06.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/endemic-simulation/static/js/2.b0b55d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343030eae583ca84dceb",
+    "url": "/endemic-simulation/static/js/main.5909c06c.chunk.js"
+  },
+  {
+    "revision": "79454d4972e051128dcd",
+    "url": "/endemic-simulation/static/js/runtime-main.c50c76e0.js"
+  }
+]);
